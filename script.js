63d1763763eff99acce8ce4c3791fd3896d4942b
Bhanu Pratap Singh <57828021+codeingroom@users.noreply.github.com>
2023-09-27 06:40:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // script.js
 const arrow = document.getElementById("arrow");
+const labels = document.querySelectorAll(".label");
 
 function handleOrientation(event) {
     const alpha = event.alpha; // Device orientation in degrees
 
     if (alpha !== null) {
-        // Calculate the direction (North, South, East, West) based on the alpha value
+        // Calculate the direction (North, East, South, West) based on the alpha value
         let direction = "North";
 
         if (alpha >= 45 && alpha < 135) {
@@ -17,7 +18,15 @@ function handleOrientation(event) {
         }
 
         arrow.style.transform = `translate(-50%, -50%) rotate(${alpha}deg)`;
-        arrow.innerText = direction;
+
+        // Remove highlight from all labels
+        labels.forEach((label) => {
+            label.classList.remove("highlight");
+        });
+
+        // Highlight the corresponding label
+        const labelElement = document.getElementById(direction.toLowerCase());
+        labelElement.classList.add("highlight");
     }
 }
 
